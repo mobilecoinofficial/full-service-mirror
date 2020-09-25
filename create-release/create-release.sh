cdcb68dba6c9f5f8763aebb3e17660c9314eeaa8
sugargoat <sugargoat@mobilecoin.com>
2020-09-24 21:19:08 -0700

    Make version agnostic

--- create-release/create-release.sh
+++ create-release/create-release.sh
@@ -7,6 +7,7 @@ if [ -z "$RELEASE_NAME" ]; then
     echo "Usage: $0 [release name, e.g. mobilecoind-mirror-0.6.0]"
     exit 1
 fi
+: VERSION=${VERSION:?"Must provide VERSION for to substitute for db paths"}
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJECT_ROOT="$SCRIPT_DIR/.."
@@ -18,6 +19,9 @@ cargo build --manifest-path $PROJECT_ROOT/Cargo.toml --release
 # Create release dir
 mkdir $RELEASE_NAME
 cp -R $SCRIPT_DIR/package/* $RELEASE_NAME
+sed -i 's/VERSION/'${VERSION}'/g' $RELEASE_NAME/mobilecoind-mirror-private.sh
+sed -i 's/VERSION/'${VERSION}'/g' $RELEASE_NAME/mobilecoind-mirror-private-tls.sh
+sed -i 's/VERSION/'${VERSION}'/g' $RELEASE_NAME/mobilecoind-mirror-private-tls-encrypted.sh
 cp $PROJECT_ROOT/mobilecoin/target/release/mobilecoind $RELEASE_NAME/bin/
 cp $PROJECT_ROOT/mobilecoin/target/release/mobilecoind-json $RELEASE_NAME/bin/
 cp $PROJECT_ROOT/target/release/mobilecoind-mirror-private $RELEASE_NAME/bin/
