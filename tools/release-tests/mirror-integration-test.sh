8e468f887958b15e9e4e9c5c4c11eedf2f3bb922
sugargoat <sugargoat@mobilecoin.com>
2020-09-24 17:25:30 -0700

    Adding endpoints

--- tools/release-tests/mirror-integration-test.sh
+++ tools/release-tests/mirror-integration-test.sh
@@ -11,7 +11,8 @@ trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
 source "$HOME/.cargo/env"
 
-pushd "$(dirname "$0")"
+RELEASE_DIR="$(dirname "$0")"
+pushd ${RELEASE_DIR}
 
 pushd ../../
 MIRROR_ROOT=$(pwd)
@@ -21,6 +22,8 @@ MOBILECOIN_ROOT=$(pwd)/mobilecoin
 SGX_MODE=${SGX_MODE:-HW}
 IAS_MODE=${IAS_MODE:-PROD}
 NAMESPACE=${NAMESPACE:-test}
+: ENTROPY=${ENTROPY:?"Must provide existing ENTROPY with balance for these tests."}
+
 
 pushd ${MOBILECOIN_ROOT}
 
@@ -31,19 +34,21 @@ curl -O https://enclave-distribution.${NAMESPACE}.mobilecoin.com/${SIGSTRUCT_URI
 TARGETDIR=${MOBILECOIN_ROOT}/target/release
 
 echo "Building mobilecoind and mobilecoind-json"
-SGX_MODE=${SGX_MODE} IAS_MODE=${IAS_MODE} \
-  CONSENSUS_ENCLAVE_CSS=$(pwd)/consensus-enclave.css \
-  cargo build --release -p mc-mobilecoind -p mc-mobilecoind-json
+#SGX_MODE=${SGX_MODE} IAS_MODE=${IAS_MODE} \
+#  CONSENSUS_ENCLAVE_CSS=$(pwd)/consensus-enclave.css \
+#  cargo build --release -p mc-mobilecoind -p mc-mobilecoind-json
 
 popd
 
 echo "Building mobilecoind-mirror"
-cargo build --release -p mc-mobilecoind-mirror \
-  --bin mobilecoind-mirror-privagte \
-  --bin mobilecoind-mirror-public
+#cargo build --release -p mc-mobilecoind-mirror \
+#  --bin mobilecoind-mirror-private \
+#  --bin mobilecoind-mirror-public
+
+MIRROR_TARGETDIR=${MIRROR_ROOT}/target/release
 
 # Remove artifacts from previous test runs
-rm -rf /tmp/ledger-db /tmp/mobilecoind-db $(pwd)/mobilecoin.log $(pwd)/mobilecoind-json.log
+# rm -rf /tmp/ledger-db /tmp/mobilecoind-db $(pwd)/mobilecoin.log $(pwd)/mobilecoind-json.log
 
 # Start mobilecoind
 echo "Starting local mobilecoind using ${NAMESPACE} servers for source of ledger. Check log at $(pwd)/mobilecoind.log."
@@ -52,7 +57,7 @@ ${TARGETDIR}/mobilecoind \
         --poll-interval 10 \
         --peer mc://node1.${NAMESPACE}.mobilecoin.com/ \
         --peer mc://node2.${NAMESPACE}.mobilecoin.com/ \
-        --tx-source-url https://s3-us-west-1.amazonaws.com/mobilecoin.chain/node1.${NAMESAPCE}.mobilecoin.com/ \
+        --tx-source-url https://s3-us-west-1.amazonaws.com/mobilecoin.chain/node1.${NAMESPACE}.mobilecoin.com/ \
         --tx-source-url https://s3-us-west-1.amazonaws.com/mobilecoin.chain/node2.${NAMESPACE}.mobilecoin.com/ \
         --mobilecoind-db /tmp/mobilecoind-db \
         --listen-uri insecure-mobilecoind://127.0.0.1:4444/ &> $(pwd)/mobilecoind.log &
@@ -65,30 +70,196 @@ ${TARGETDIR}/mobilecoind-json &> $(pwd)/mobilecoind-json.log &
 
 mj_pid=$!
 
+# Wait for mobilecoind-json to be live
+sleep 2
+
+source ${RELEASE_DIR}/assert.sh
+
+failure_count=0
+
 # Wait for mobilecoind to sync the ledger.
-block_height=$(curl localhost:9090/ledger/local | jq)
-echo $block_height
+block_height=$(curl -s localhost:9090/ledger/local)
+if [[ $(echo $block_height | jq '.block_count?') != 'null' ]] \
+  && [[ $(echo $block_height | jq '.txo_count?') != 'null' ]]; then
+  log_success "block_height"
+else
+  log_failure "block_height"
+  failure_count=$(( $failure_count + 1 ))
+fi
 
-# Helper method to assert equalities
-assert_eq() {
-  local expected="$1"
-  local actual="$2"
-  local msg
+# FIXME: assert block_height > 0?
 
-  if [ "$#" -ge 3 ]; then
-    msg="$3"
-  fi
+# Generate a new entropy
+entropy=$(curl -s localhost:9090/entropy -X POST)
+if [[ $(echo $entropy | jq '.entropy?') != 'null' ]]; then
+  log_success "entropy"
+else
+  log_failure "entropy"
+  failure_count=$(( $failure_count + 1 ))
+fi
 
-  if [ "$expected" == "$actual" ]; then
-    return 0
-  else
-    [ "${#msg}" -gt 0 ] && log_failure "$expected == $actual :: $msg" || true
-    return 1
-  fi
-}
+# add and remove dummy entropy
+# remove=$(curl -s -X DELETE localhost:9090/monitors/$monitor_id)
+# FIXME: Try to remove twice and see that it fails
 
-# Test block endpoint
-expected='{"block_id":"e498010ee6a19b4ac9313af43d8274c53d54a1bbc275c06374dbe0095872a6ee","version":0,"parent_id":"0000000000000000000000000000000000000000000000000000000000000000","index":"0","cumulative_txo_count":"10000","contents_hash":"40bffaff21f4825bc36e4598c3346b375fe77ec1c78f15c8a98623c0ba6b1d21"}'
+# Generate account key from entropy - Use an the provided ENTROPY with a balance on the NAMESPACE network
+account_key=$(curl -s localhost:9090/entropy/$ENTROPY)
+if [[ $(echo $account_key | jq '.view_private_key?') != 'null' ]] && [[ $(echo $account_key | jq '.spend_private_key?') != 'null' ]]; then
+  log_success "account_key"
+else
+  log_failure "account_key"
+  failure_count=$(( $failure_count + 1 ))
+fi
+
+# Add a monitor for a key over a range of subaddress indices
+monitor=$(curl -s localhost:9090/monitors \
+  -d '{"account_key": '$account_key',
+       "first_subaddress": 0, "num_subaddresses": 10}' \
+  -X POST -H 'Content-Type: application/json')
+if [[ $(echo $monitor | jq '.monitor_id?') != 'null' ]]; then
+  log_success "monitor_id"
+else
+  log_failure "monitor_id"
+  failure_count=$(( $failure_count + 1 ))
+fi
+
+monitor_id=$(echo $monitor | jq -r '.monitor_id')
+
+# Get the status of an existing monitor
+status=$(curl -s localhost:9090/monitors/$monitor_id)
+if [[ $(echo $status | jq '.first_subaddress?') != 'null' ]] \
+  && [[ $(echo $status | jq '.num_subaddresses?') != 'null' ]] \
+  && [[ $(echo $status | jq '.first_block?') != 'null' ]] \
+  && [[ $(echo $status | jq '.next_block?') != 'null' ]]; then
+  log_success "monitor status"
+else
+  log_failure "monitor status"
+  failure_count=$(( $failure_count + 1 ))
+fi
+
+# Check balance
+balance=$(curl -s localhost:9090/monitors/$monitor_id/subaddresses/0/balance)
+if [[ $(echo $balance | jq '.balance?') != 'null' ]] \
+  && [[ $(( $(echo $balance | jq -r '.balance') > 0)) == 1 ]]; then
+  log_success "balance"
+else
+  log_failure "balance"
+  failure_count=$(( $failure_count + 1 ))
+fi
+
+public_address=$(curl -s localhost:9090/monitors/$monitor_id/subaddresses/0/public-address)
+if [[ $(echo $public_address | jq '.view_public_key?') != 'null' ]] \
+  && [[ $(echo $public_address | jq '.spend_public_key?') != 'null' ]] \
+  && [[ $(echo $public_address | jq '.fog_report_url?') != 'null' ]] \
+  && [[ $(echo $public_address | jq '.fog_authority_fingerprint_sig?') != 'null' ]] \
+  && [[ $(echo $public_address | jq '.fog_report_id?') != 'null' ]] \
+  && [[ $(echo $public_address | jq '.b58_address_code?') != 'null' ]]; then
+  log_success "pubic address"
+else
+  log_failure "public address"
+  failure_count=$(( $failure_count + 1 ))
+fi
+
+address_code=$(echo $public_address | jq -r '.b58_address_code')
+pay=$(curl -s http://localhost:9090/monitors/$monitor_id/subaddresses/0/pay-address-code \
+  -d '{"receiver_b58_address_code": "'$address_code'", "value": "1"}' \
+  -X POST -H 'Content-Type: application/json')
+if [[ $(echo $pay | jq '.sender_tx_receipt?') != 'null' ]] \
+  && [[ $(echo $pay | jq '.receiver_tx_receipt_list?') != 'null' ]]; then
+  log_success "pay"
+else
+  log_failure "pay"
+  failure_count=$(( $failure_count + 1 ))
+fi
+
+status=$(curl -s localhost:9090/tx/status-as-sender -d $pay -X POST -H 'Content-Type: application/json')
+if [[ $(echo $status | jq '.status?') != 'null' ]] \
+  && [[ $(echo $status | jq '.status? == "verified"') == true ]]; then
+  log_success "status"
+else
+  log_failure "status"
+  failure_count=$(( $failure_count + 1 ))
+fi
+
+receipt=$(echo $pay | jq -c '.receiver_tx_receipt_list[0]')
+status=$(curl -s localhost:9090/tx/status-as-receiver \
+  -d $receipt -X POST -H 'Content-Type: application/json')
+if [[ $(echo $status | jq '.status?') != 'null' ]] \
+  && [[ $(echo $status | jq '.status? == "verified"') == true ]]; then
+  log_success "status"
+else
+  log_failure "status"
+  failure_count=$(( $failure_count + 1 ))
+fi
+
+code=$(curl -s localhost:9090/codes/request \
+  -d '{"receiver": '$public_address', "value": "10", "memo": "Test"}'\
+  -X POST -H 'Content-Type: application/json')
+if [[ $(echo $code | jq '.b58_code?') != 'null' ]]; then
+  log_success "code"
+else
+  log_failure "code"
+  failure_count=$(( $failure_count + 1 ))
+fi
+
+request_code=$(echo $code | jq -r '.b58_code')
+request_data=$(curl -s localhost:9090/codes/request/$request_code)
+if [[ $(echo $request_data | jq '.receiver?') != 'null' ]] \
+  && [[ $(echo $request_data | jq '.value?') != 'null' ]] \
+  && [[ $(echo $request_data | jq '.memo?') != 'null' ]]; then
+  log_success "status"
+else
+  log_failure "status"
+  failure_count=$(( $failure_count + 1 ))
+fi
+
+submit=$(curl -s localhost:9090/monitors/$monitor_id/subaddresses/0/build-and-submit \
+  -d '{"request_data": '$request_data'}' \
+  -X POST -H 'Content-Type: application/json')
+if [[ $(echo $submit | jq '.sender_tx_receipt?') != 'null' ]] \
+  && [[ $(echo $submit | jq '.receiver_tx_receipt_list?') != 'null' ]]; then
+  log_success "submit"
+else
+  log_failure "submit"
+  failure_count=$(( $failure_count + 1 ))
+fi
+
+# Counts for a specific block
+counts=$(curl -s localhost:9090/ledger/blocks/1/header)
+if [[ $(echo $counts | jq '.key_image_count?') != 'null' ]] \
+  && [[ $(echo $counts | jq '.txo_count?') != 'null' ]]; then
+  log_success "counts"
+else
+  log_failure "counts"
+  failure_count=$(( $failure_count + 1 ))
+fi
+
+# Details for a specific block
+details=$(curl -s localhost:9090/ledger/blocks/1)
+if [[ $(echo $details | jq '.block_id?') != 'null' ]] \
+  && [[ $(echo $details | jq '.version?') != 'null' ]] \
+  && [[ $(echo $details | jq '.parent_id?') != 'null' ]] \
+  && [[ $(echo $details | jq '.index?') != 'null' ]] \
+  && [[ $(echo $details | jq '.cumulative_txo_count?') != 'null' ]] \
+  && [[ $(echo $details | jq '.contents_hash?') != 'null' ]]; then
+  log_success "details"
+else
+  log_failure "details"
+  failure_count=$(( $failure_count + 1 ))
+fi
+
+# Offline UTXOS
+utxos=$(curl -s localhost:9090/monitors/$monitor_id/subaddresses/0/utxos)
+# FIXME: check contents
+
+utxo=$(echo $utxos | jq '.output_list[0]')
+propsoal=$(curl -s localhost:9090/monitors/$monitor-id/subaddresses/0/generate-tx \
+  -d '{"input_list": ['$utxo'], "transfer": '$request_data'}' \
+  -X POST -H 'Content-Type: application/json')
+
+for pid in $m_pid $mj_pid; do
+  wait  $pid
+done
 
 popd
 
