40aa29b7ebca70a84b0a23e6be7e0daac8665809
William J <william@mobilecoin.com>
2022-03-16 16:11:23 -0400

    Removing unintended change to example client

--- example-client.js
+++ example-client.js
@@ -4,7 +4,7 @@ const client = require('./test/test_lib/send-request-encrypted');
 // Command line parsing
 if (process.argv.length != 6) {
     console.log(`Usage: node example-client.js <public mirror host> <public mirror port> <key file> <request>`);
-    console.log(`For example: node example-client.js 127.0.0.1 9091 mirror-client.pem '{"GetBlock": {"block": 0}}'`);
+    console.log(`For example: node example-client.js 127.0.0.1 9091 mirror-client.pem '{"method": "get_block", "params": {"block_index": "0"}, "jsonrpc": "2.0", "id": 1}'`);
     console.log('To generate keys please run the generate-rsa-keypair binary. See README.md for more details')
     return;
 }
